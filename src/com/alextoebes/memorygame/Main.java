300e4496c08c45a5516fb5a100cdb073ee760213
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 15:33:11 +0200

    Improved styling

--- src/com/alextoebes/memorygame/Main.java
+++ src/com/alextoebes/memorygame/Main.java
@@ -2,8 +2,9 @@ package com.alextoebes.memorygame;
 
 import com.alextoebes.memorygame.view.Container;
 import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Container root = FXMLLoader.load(getClass().getResource("/fxml/container.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Memory");
         primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.setResizable(false);
         primaryStage.show();
