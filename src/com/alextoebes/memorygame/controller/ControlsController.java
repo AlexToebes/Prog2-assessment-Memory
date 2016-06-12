608a5f59cf756d22af7f71d5fe290d9fd4348a3d
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 16:54:02 +0200

    Made name and score fields available in controller

--- src/com/alextoebes/memorygame/controller/ControlsController.java
+++ src/com/alextoebes/memorygame/controller/ControlsController.java
@@ -1,7 +1,15 @@
 package com.alextoebes.memorygame.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
 /**
  * Created by alextoebes on 12/06/16.
  */
 public class ControlsController extends Controller {
+    @FXML private TextField player1Field;
+    @FXML private HBox player1Score;
+    @FXML private TextField player2Field;
+    @FXML private HBox player2Score;
 }
