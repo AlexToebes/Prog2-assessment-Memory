6e22e6628e5191231b854431220615da616a3e43
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 17:53:28 +0200

    Cleared some unused code

--- src/com/alextoebes/memorygame/Main.java
+++ src/com/alextoebes/memorygame/Main.java
@@ -1,18 +1,16 @@
 package com.alextoebes.memorygame;
 
-import com.alextoebes.memorygame.view.Container;
 import javafx.application.Application;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Container root = FXMLLoader.load(getClass().getResource("/fxml/container.fxml"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("/fxml/container.fxml"));
         primaryStage.setTitle("Memory");
         primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.setResizable(false);
