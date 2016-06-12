a15f651b7ed29324b7c888fc627228cef4aabead
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 16:53:14 +0200

    Made nested controllers

--- src/com/alextoebes/memorygame/controller/ApplicationController.java
+++ src/com/alextoebes/memorygame/controller/ApplicationController.java
@@ -1,8 +1,16 @@
 package com.alextoebes.memorygame.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+
 /**
  * Created by alextoebes on 12/06/16.
  */
 public class ApplicationController extends Controller {
+    @FXML private MenuBar menuBar;
+    @FXML private BorderPane content;
+    @FXML private MenuBarController menuBarController;
+    @FXML private GameController contentController;
 }
 
--- src/com/alextoebes/memorygame/controller/GameController.java
+++ src/com/alextoebes/memorygame/controller/GameController.java
@@ -1,7 +1,15 @@
 package com.alextoebes.memorygame.controller;
 
+import com.alextoebes.memorygame.view.BoardPane;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 /**
  * Created by alextoebes on 12/06/16.
  */
 public class GameController extends Controller {
+    @FXML private BoardPane board;
+    @FXML private VBox controls;
+    @FXML private BoardController boardController;
+    @FXML private ControlsController controlsController;
 }
