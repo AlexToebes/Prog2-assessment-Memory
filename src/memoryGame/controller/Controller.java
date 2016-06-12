456918eda083265cf1fc33cc85cd6e5606422c71
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 10:08:09 +0200

    Improved folder structure

---
+++ src/memoryGame/controller/Controller.java
@@ -0,0 +1,9 @@
+package memoryGame.controller;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void buttonClicked(ActionEvent actionEvent) {
+        System.out.printf("Clicked %n");
+    }
+}
