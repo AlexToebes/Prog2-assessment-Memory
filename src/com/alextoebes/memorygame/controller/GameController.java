feedbe4f81c6b07a453b20da2e3d372f15d0b454
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 11:43:35 +0200

    Fixed errors shown by button

--- src/com/alextoebes/memorygame/controller/GameController.java
+++ src/com/alextoebes/memorygame/controller/GameController.java
@@ -7,9 +7,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
-    public void buttonClicked(ActionEvent actionEvent) {
-        System.out.printf("Clicked %n");
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
