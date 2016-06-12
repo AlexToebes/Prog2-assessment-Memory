df4cb0857ad5bba897946b62cd708fc5e88dfb28
Alex Toebes <alex.toebes@gmail.com>
2016-06-12 16:35:47 +0200

    Created abstract controller which implements Initializable

---
+++ src/com/alextoebes/memorygame/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.alextoebes.memorygame.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by alextoebes on 12/06/16.
+ */
+public abstract class Controller implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
