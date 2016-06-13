2b768804e994cb98a468c9e5322eb4509f831167
Alex Toebes <alex.toebes@gmail.com>
2016-06-13 21:33:47 +0200

    created onDocumentReady, which fires after rendering javafx

--- src/com/alextoebes/memorygame/Main.java
+++ src/com/alextoebes/memorygame/Main.java
@@ -6,7 +6,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Main extends Application {
+
+    protected static List<ActionListener> listenerList = new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,10 +23,22 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.setResizable(false);
         primaryStage.show();
+        this.triggerDocumentReady();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void onDocumentReady(ActionListener listener) {
+        listenerList.add(listener);
+    }
+
+    private void triggerDocumentReady() {
+        ActionEvent event = new ActionEvent(this, UUID.randomUUID().hashCode(), "Document ready");
+        for (ActionListener listener:listenerList) {
+            listener.actionPerformed(event);
+        }
     }
 }
