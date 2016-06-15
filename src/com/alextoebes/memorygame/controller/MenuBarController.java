85543a799e2630090151221341badbbd8eade2f0
Alex Toebes <alex.toebes@gmail.com>
2016-06-15 09:46:42 +0200

    Added shortcuts and methods for menu items

--- src/com/alextoebes/memorygame/controller/MenuBarController.java
+++ src/com/alextoebes/memorygame/controller/MenuBarController.java
@@ -1,7 +1,24 @@
 package com.alextoebes.memorygame.controller;
 
+import javafx.event.ActionEvent;
+
 /**
  * Created by alextoebes on 12/06/16.
  */
 public class MenuBarController extends Controller {
+
+    public void handleLoad(ActionEvent actionEvent) {
+        // TODO: 15/06/16 implementation
+        System.out.println("handleLoad");
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+        // TODO: 15/06/16 implementation
+        System.out.println("handleSave");
+    }
+
+    public void handleExit(ActionEvent actionEvent) {
+        // TODO: 15/06/16 implementation 
+        System.out.println("handleExit");
+    }
 }
